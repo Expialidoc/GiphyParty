9de7152e1669b114bd5c035af9296b534ca69329
Vladimir Pogorelov <vpogorelov1@gmail.com>
2021-10-06 23:05:26 -0400

    update

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 console.log("Let's get this party started!");
 //First create account and get the key in dashboard!
-let key = "nM36br5iO0hoF1RVcagPPCVZT9zpyiNE";
-async function getGif(descrip,key){
-    const res = await axios.get('http://api.giphy.com/v1/gifs/search', {params:{q: descrip,api_key: key}});
-//    api_key:nM36br5iO0hoF1RVcagPPCVZT9zpyiNE    
-    const img = document.querySelector("#gif");
-    document.querySelector('.getGif').append(img);
-  img.src = res.data.images;
+//key = "nM36br5iO0hoF1RVcagPPCVZT9zpyiNE";
+const gifs = document.querySelector('.getGif');
+
+function getGif(response){
+  let newGif = document.createElement('img'); newGif.style.width = '33%';newGif.style.height = 'auto';
+
+    gifs.append(newGif);
+      if(response.data.length){
+        let randInd = Math.floor(Math.random()*response.data.length);
+        newGif.src = response.data[randInd].images.original.url;
+      }
 }
 
 const form = document.querySelector('#form');
-form.addEventListener("submit", function (e) {
-  const input = document.querySelector('#search');
-  e.preventDefault();
-  getGif(input.value, key);//console.log(input.value);
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    let input = document.querySelector('#search'); 
+    let descrip = input.value;
+
   input.value = '';
+  const res = await axios.get('http://api.giphy.com/v1/gifs/search', {params:
+  {q: descrip, api_key: 'nM36br5iO0hoF1RVcagPPCVZT9zpyiNE'}});
+    getGif(res.data);
 })
 
 const btn = document.querySelector('#remove');
-const gifs = document.querySelectorAll('#gif');
     btn.addEventListener("click", function(){
-        gifs.forEach(e => e.remove());
-    })
+      while (gifs.firstChild) {
+        gifs.removeChild(gifs.firstChild);
+    }
+})
